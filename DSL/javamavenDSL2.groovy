fd925bd0930cd98230c50f68319950966868cb86
YishayRE <62124799+YishayRE@users.noreply.github.com>
2023-10-19 00:10:15 -0600

    Update javamavenDSL2.groovy

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,18 +1,18 @@
 job('Java Maven App DSL 2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/YishayRE/Maven-example.git', 'main') { node ->
+            node / gitConfigName('YishayRE')
+            node / gitConfigEmail('yishel.miguel@gmail.com')
         }
     }
     steps {
         maven {
-          mavenInstallation('mavenjenkins')
+          mavenInstallation('Maven Jenkins')
           goals('-B -DskipTests clean package')
         }
         maven {
-          mavenInstallation('mavenjenkins')
+          mavenInstallation('Maven Jenkins')
           goals('test')
         }
         shell('''
